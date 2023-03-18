fada4eb3e67d4d968e83892c5472c72a46869faa
peponi <peponi_paradise@naver.com>
2023-03-18 20:36:40 +0900

    Image filter upload

---
+++ OpenCVSharp4/ImageFilter/ImageFilter/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ImageFilter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ExecuteFilter2D = new System.Windows.Forms.Button();
+            this.ExecuteGaussianBlur = new System.Windows.Forms.Button();
+            this.ExecuteBoxFilter = new System.Windows.Forms.Button();
+            this.ExecuteMedianBlur = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.PictureBox();
+            this.Result = new System.Windows.Forms.PictureBox();
+            this.ExecuteBilateralFilter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Result)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ExecuteFilter2D
+            // 
+            this.ExecuteFilter2D.Location = new System.Drawing.Point(12, 12);
+            this.ExecuteFilter2D.Name = "ExecuteFilter2D";
+            this.ExecuteFilter2D.Size = new System.Drawing.Size(185, 38);
+            this.ExecuteFilter2D.TabIndex = 2;
+            this.ExecuteFilter2D.Text = "Filter2D";
+            this.ExecuteFilter2D.UseVisualStyleBackColor = true;
+            // 
+            // ExecuteGaussianBlur
+            // 
+            this.ExecuteGaussianBlur.Location = new System.Drawing.Point(394, 12);
+            this.ExecuteGaussianBlur.Name = "ExecuteGaussianBlur";
+            this.ExecuteGaussianBlur.Size = new System.Drawing.Size(185, 38);
+            this.ExecuteGaussianBlur.TabIndex = 2;
+            this.ExecuteGaussianBlur.Text = "Gaussian Blur";
+            this.ExecuteGaussianBlur.UseVisualStyleBackColor = true;
+            // 
+            // ExecuteBoxFilter
+            // 
+            this.ExecuteBoxFilter.Location = new System.Drawing.Point(203, 12);
+            this.ExecuteBoxFilter.Name = "ExecuteBoxFilter";
+            this.ExecuteBoxFilter.Size = new System.Drawing.Size(185, 38);
+            this.ExecuteBoxFilter.TabIndex = 2;
+            this.ExecuteBoxFilter.Text = "Box Filter";
+            this.ExecuteBoxFilter.UseVisualStyleBackColor = true;
+            // 
+            // ExecuteMedianBlur
+            // 
+            this.ExecuteMedianBlur.Location = new System.Drawing.Point(585, 12);
+            this.ExecuteMedianBlur.Name = "ExecuteMedianBlur";
+            this.ExecuteMedianBlur.Size = new System.Drawing.Size(185, 38);
+            this.ExecuteMedianBlur.TabIndex = 2;
+            this.ExecuteMedianBlur.Text = "Median Blur";
+            this.ExecuteMedianBlur.UseVisualStyleBackColor = true;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 100);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(376, 338);
+            this.Input.TabIndex = 3;
+            this.Input.TabStop = false;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(394, 100);
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(376, 338);
+            this.Result.TabIndex = 3;
+            this.Result.TabStop = false;
+            // 
+            // ExecuteBilateralFilter
+            // 
+            this.ExecuteBilateralFilter.Location = new System.Drawing.Point(12, 56);
+            this.ExecuteBilateralFilter.Name = "ExecuteBilateralFilter";
+            this.ExecuteBilateralFilter.Size = new System.Drawing.Size(185, 38);
+            this.ExecuteBilateralFilter.TabIndex = 2;
+            this.ExecuteBilateralFilter.Text = "Bilateral Filter";
+            this.ExecuteBilateralFilter.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.ExecuteMedianBlur);
+            this.Controls.Add(this.ExecuteBoxFilter);
+            this.Controls.Add(this.ExecuteGaussianBlur);
+            this.Controls.Add(this.ExecuteBilateralFilter);
+            this.Controls.Add(this.ExecuteFilter2D);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Result)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ExecuteFilter2D;
+        private System.Windows.Forms.Button ExecuteGaussianBlur;
+        private System.Windows.Forms.Button ExecuteBoxFilter;
+        private System.Windows.Forms.Button ExecuteMedianBlur;
+        private System.Windows.Forms.PictureBox Input;
+        private System.Windows.Forms.PictureBox Result;
+        private System.Windows.Forms.Button ExecuteBilateralFilter;
+    }
+}
+
---
+++ OpenCVSharp4/ImageFilter/ImageFilter/Form1.cs
@@ -0,0 +1,179 @@
+﻿using OpenCvSharp;
+using System;
+using System.Windows.Forms;
+
+namespace ImageFilter
+{
+    public partial class Form1 : Form
+    {
+        private enum FilterType
+        {
+            Filter2D,
+            Box,
+            Gaussian,
+            Median,
+            Bilateral
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Set tags for click event
+            ExecuteFilter2D.Tag = FilterType.Filter2D;
+            ExecuteBoxFilter.Tag = FilterType.Box;
+            ExecuteGaussianBlur.Tag = FilterType.Gaussian;
+            ExecuteMedianBlur.Tag = FilterType.Median;
+            ExecuteBilateralFilter.Tag = FilterType.Bilateral;
+
+            // Set click event
+            ExecuteFilter2D.Click += ExecuteFilter;
+            ExecuteBoxFilter.Click += ExecuteFilter;
+            ExecuteGaussianBlur.Click += ExecuteFilter;
+            ExecuteMedianBlur.Click += ExecuteFilter;
+            ExecuteBilateralFilter.Click += ExecuteFilter;
+        }
+
+        private void ExecuteFilter(object sender, EventArgs e)
+        {
+            if (FileOpen(out var image))
+            {
+                Mat rtn = new Mat();
+                switch ((FilterType)(sender as Control).Tag)
+                {
+                    case FilterType.Filter2D:
+                        rtn = Filter2D(image);
+                        break;
+
+                    case FilterType.Box:
+                        rtn = BoxFilter(image);
+                        break;
+
+                    case FilterType.Gaussian:
+                        rtn = GaussianFilter(image);
+                        break;
+
+                    case FilterType.Median:
+                        rtn = MedianFilter(image);
+                        break;
+
+                    case FilterType.Bilateral:
+                        rtn = BilateralFilter(image);
+                        break;
+                }
+                ShowImage(image, rtn);
+            }
+        }
+
+        private bool FileOpen(out Mat image)
+        {
+            image = default;
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Mat(dialog.FileName);
+            }
+            return image != null ? !image.Empty() : false;
+        }
+
+        /// <summary>
+        /// 필터 또는 샤프닝 효과
+        /// </summary>
+        /// <param name="input">원본 이미지</param>
+        /// <returns>결과 이미지</returns>
+        private Mat Filter2D(Mat input)
+        {
+            Mat rtn = new Mat();
+
+            // 아래 마스크에 지정하는 공간 크기만큼 배열 할당
+            // 필터 또는 샤프닝 효과를 얻는데 사용 가능
+            // 샤프닝 효과를 얻을 경우 중앙 (타겟 픽셀) 값은 양수, 나머지는 음수로 하고 마스크 총 합이 1이 되도록 한다.
+            float[] maskWeight = new float[9] { 0, -0.25F, 0,
+                                                       -0.25F, 2, -0.25F,
+                                                       0, -0.25F, 0 };
+            Mat mask = new Mat(3, 3, MatType.CV_32FC1, maskWeight);
+
+            Cv2.Filter2D(input, rtn, input.Type(), mask);
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 필터 효과를 얻을 수 있음<br/>
+        /// BoxFilter method의 normalize 옵션<br/>
+        /// 1. true (default) : 모든 마스크 가중치 값이 1/커널 크기<br/>
+        /// 2. false : 모든 마스크 가중치 값이 1
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private Mat BoxFilter(Mat input)
+        {
+            Mat rtn = new Mat();
+
+            OpenCvSharp.Size kernelSize = new OpenCvSharp.Size(3, 3);
+
+            Cv2.BoxFilter(input, rtn, input.Type(), kernelSize, normalize: false);
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 가우시안 함수를 이용하여 이미지 블러링<br/>
+        /// 커널 축의 길이는 홀수로 정의됨<br/>
+        /// 너무 심하게 적용하면 엣지 훼손될 수 있음
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private Mat GaussianFilter(Mat input)
+        {
+            Mat rtn = new Mat();
+
+            // sigmaX and Y : 해당 축 방향 표준편차 (0 : 자동), 값이 커질수록 더 많이 블러링
+            Cv2.GaussianBlur(input, rtn, new OpenCvSharp.Size(3, 3), 0, 0);
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 모든 마스크 값을 1로하고, 크기만 지정하여 이미지 블러링<br/>
+        /// 노이즈를 제거하면서 가장자리를 처리하는데 유리<br/>
+        /// 특히 소금-후추 잡음 (노이즈로 인해 나타나는 비정상 픽셀값) 처리에 좋다
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private Mat MedianFilter(Mat input)
+        {
+            Mat rtn = new Mat();
+
+            Cv2.MedianBlur(input, rtn, 3);
+
+            return rtn;
+        }
+
+        /// <summary>
+        /// 타겟, 인접 픽셀의 값을 고려하여 블러링 수행<br/>
+        /// 엣지를 유지하면서 블러링을 수행할 수 있다<br/>
+        /// 계산을 위한 수식이 길어져 연산이 오래걸릴 수 있음
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private Mat BilateralFilter(Mat input)
+        {
+            Mat rtn = new Mat();
+
+            // 보통 sigmaColor 및 Space 값은 비슷하게 할당
+            Cv2.BilateralFilter(input, rtn, 3, 50, 50);
+
+            return rtn;
+        }
+
+        private void ShowImage(Mat input, Mat rtn)
+        {
+            Window win1 = new Window("Input", input);
+            Window win2 = new Window("Result", rtn);
+
+            input.Release();
+            rtn.Release();
+        }
+    }
+}
