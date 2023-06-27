80ef5ed59af90c6d4afee77bc2bdab1015443e7c
peponi <peponi_paradise@naver.com>
2023-06-27 21:07:13 +0900

    작성 중

---
+++ Language/Program.cs
@@ -0,0 +1,33 @@
+﻿// 값 형식 예제
+
+// 할당
+
+int a = 1;
+int b = a;
+a = 2;
+
+Console.WriteLine($"a = {a}, b = {b}"); // a = 2, b = 1
+
+// 메서드 인수 전달
+
+ChangeInt(b);    
+
+void ChangeInt(int val)
+{
+    val=500;
+    Console.WriteLine($"val = {val}");
+}
+
+Console.WriteLine($"b = {b}");
+
+// 메서드 리턴
+
+Console.WriteLine($"a = {ChangeAndReturnInt(a)}");
+
+Console.WriteLine($"a = {a}");
+
+int ChangeAndReturnInt(int val) 
+{
+    val=100;
+    return val;
+}
