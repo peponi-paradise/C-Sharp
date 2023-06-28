3b295638731afb828ef0f71bcff86f63f88e8e95
peponi <peponi_paradise@naver.com>
2023-06-28 11:59:08 +0900

    실수 형식까지 진행

--- Language/Program.cs
+++ Language/Program.cs
@@ -1,4 +1,6 @@
-﻿// 값 형식 예제
+﻿using System.Numerics;
+
+// 값 형식 예제
 
 // 할당
 
@@ -10,11 +12,11 @@ Console.WriteLine($"a = {a}, b = {b}"); // a = 2, b = 1
 
 // 메서드 인수 전달
 
-ChangeInt(b);    
+ChangeInt(b);
 
 void ChangeInt(int val)
 {
-    val=500;
+    val = 500;
     Console.WriteLine($"val = {val}");
 }
 
@@ -26,8 +28,52 @@ Console.WriteLine($"a = {ChangeAndReturnInt(a)}");
 
 Console.WriteLine($"a = {a}");
 
-int ChangeAndReturnInt(int val) 
+int ChangeAndReturnInt(int val)
 {
-    val=100;
+    val = 100;
     return val;
 }
+
+// 정수 100을 초기화
+
+int binaryInt = 0b_0110_0100;
+int binaryInt2 = 0b01100100;
+
+int decimalInt = 100_00;
+
+int hexInt = 0x64;
+
+var int1 = 9_999;    // int
+var int2 = 2_999_999_999;      // uint
+var int3 = 9_999_999_999;      // long
+var int4 = 9_999_999_999_999_999_999;       // ulong
+
+short short1 = 30000;
+//short short2 = 99999;   // CS0031: '99999' 상수 값을 'short`(으)로 변환할 수 없습니다.
+
+var byte1 = (byte)15;
+var long1 = (uint)15;
+
+var floatValue = 0.1F;
+
+var doubleValue = 0.1;
+var doubleValue2 = 0.1D;
+
+var decimalValue = 0.1M;
+
+double castingDouble = 1.6;
+int castingInt = (int)castingDouble;    // castingInt = 1
+
+double castingDouble2 = 1.5;
+int castingInt2 = (int)Math.Round(castingDouble2);    // castingInt2 = 2
+
+Console.WriteLine(castingInt2);
+
+double X = 1.0;
+decimal Y = 1M;
+
+double Z = X + (double)Y;
+decimal W = (decimal)X + Y;
+
+Console.WriteLine(Z);
+Console.WriteLine(W);
