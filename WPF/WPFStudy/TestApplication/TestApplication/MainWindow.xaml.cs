d4b12ed31b6cf5c3dd5985ee30fbf12e7d5ee098
peponi <peponi_paradise@naver.com>
2023-07-18 23:22:06 +0900

    WPF Study - 기본 WPF 어플 작성

---
+++ WPF/WPFStudy/TestApplication/TestApplication/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(NameInput.Text) && !string.IsNullOrWhiteSpace(CompanyInput.Text) && !string.IsNullOrWhiteSpace(PhoneNumberInput.Text))
+            {
+                var personData = new PersonData(NameInput.Text, CompanyInput.Text, PhoneNumberInput.Text);
+                if (!PersonList.Items.Contains(personData)) PersonList.Items.Add(personData);
+            }
+        }
+
+        private void ButtonRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (PersonList.SelectedItem != null) PersonList.Items.Remove(PersonList.SelectedItem);
+        }
+    }
+
+    public record PersonData
+    {
+        public string Name { get; init; }
+        public string Company { get; init; }
+        public string PhoneNumber { get; init; }
+
+        public PersonData(string name, string company, string phoneNumber)
+        {
+            Name = name;
+            Company = company;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
