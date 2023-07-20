57c4251fd97888ca9f5e8a754f197413b69ad0e3
peponi <peponi_paradise@naver.com>
2023-07-21 07:44:03 +0900

    테스트 후 수정

--- WPF/WPFStudy/SerialPort/Library/Services/SerialPort/Models/SerialPortModel.cs
+++ WPF/WPFStudy/SerialPort/Library/Services/SerialPort/Models/SerialPortModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Messaging;
 using Library.Core.Messenger;
 using System.Collections.ObjectModel;
+using System.Threading;
 
 namespace Library.Services.SerialPort;
 
@@ -9,15 +10,21 @@ public partial class SerialPortModel : ObservableObject
 {
     public ObservableCollection<string> CommunicationData;
 
+    readonly SynchronizationContext? SyncContext;
+
     public SerialPortModel()
     {
+        SyncContext = SynchronizationContext.Current;
         CommunicationData = new ObservableCollection<string>();
         WeakReferenceMessenger.Default.Register<SerialPortReceived>(this, (o, e) => UpdateCommunicationData(e.Value));
     }
 
     private void UpdateCommunicationData(string communicationData)
     {
-        CommunicationData.Add(communicationData);
-        if (CommunicationData.Count > 100) CommunicationData.RemoveAt(0);
+        SyncContext?.Post(delegate
+        {
+            CommunicationData.Add(communicationData);
+            if (CommunicationData.Count > 100) CommunicationData.RemoveAt(0);
+        }, null);
     }
 }
