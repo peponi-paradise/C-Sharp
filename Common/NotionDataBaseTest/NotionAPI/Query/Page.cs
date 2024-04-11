3995b2a381dda800449f4439935989477aaef947
peponi <peponi_paradise@naver.com>
2024-04-11 16:23:39 +0900

    Page query 테스트

--- Common/NotionDataBaseTest/NotionAPI/Query/Page.cs
+++ Common/NotionDataBaseTest/NotionAPI/Query/Page.cs
@@ -3,9 +3,12 @@
 namespace NotionAPI.Objects;
 
 // https://developers.notion.com/reference/post-page 에 따라 작성
-public class CreatePageItem
+public class QueryPageItem
 {
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Parent? parent { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public Dictionary<string, PageProperty>? properties { get; set; }
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
@@ -16,4 +19,7 @@ public class CreatePageItem
 
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public object? cover { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public bool? archived { get; set; }
 }
--- Common/NotionDataBaseTest/PageQuery/Program.cs
+++ Common/NotionDataBaseTest/PageQuery/Program.cs
@@ -10,34 +10,112 @@ namespace PageQuery
         {
             string baseUri = $"https://api.notion.com/v1/pages";
             string databaseKey = "데이터베이스 키";
+            string pageKey = "Page 키";
             string APIKey = "API 키";
 
             Console.WriteLine(Create(baseUri, databaseKey, APIKey));
+            Console.WriteLine(Retrieve(baseUri, pageKey, APIKey));
+            Console.WriteLine(Update(baseUri, pageKey, APIKey));
+            Console.WriteLine(Archieve(baseUri, pageKey, APIKey));
         }
 
         private static bool Create(string baseUri, string databaseKey, string APIKey)
         {
             HttpClient client = new();
 
+            // https://developers.notion.com/reference/post-page 문서 내용에 따라 Post로 request 작성
             var request = new HttpRequestMessage(HttpMethod.Post, $"{baseUri}");
             request.Headers.Add("Authorization", $"Bearer {APIKey}");
             request.Headers.Add("Notion-Version", "2022-06-28");
-            var addItem = new CreatePageItem();
-            addItem.parent = new DatabaseParent() { database_id = databaseKey };
+            var queryItem = new QueryPageItem();
+            queryItem.parent = new DatabaseParent() { database_id = databaseKey };
             var properties = new Dictionary<string, PageProperty>
             {
                 { "이름", new PageTitle() { title = [new RichTextWithText() { text = new() { content = "5" } }] } },
                 { "선택", new PageSelect() { select = new() { name = "2" } } }
             };
-            addItem.properties = properties;
-            request.Content = JsonContent.Create(addItem, null, new JsonSerializerOptions() { IncludeFields = true });
+            queryItem.properties = properties;
+            request.Content = JsonContent.Create(queryItem);
 
             var response = client.Send(request);
 
-            // StatusCode를 포함한 HTTP response 출력
+            // StatusCode를 포함한 Header 출력
             Console.WriteLine(response);
-            // JSON 형식의 response data 출력
+            // JSON 형식의 Body 출력
             Console.WriteLine(new StreamReader(response.Content.ReadAsStream()).ReadToEnd());
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        private static bool Retrieve(string baseUri, string pageKey, string APIKey)
+        {
+            HttpClient client = new();
+
+            // https://developers.notion.com/reference/retrieve-a-page 문서 내용에 따라 Get으로 request 작성
+            var request = new HttpRequestMessage(HttpMethod.Get, $"{baseUri}/{pageKey}");
+            request.Headers.Add("Authorization", $"Bearer {APIKey}");
+            request.Headers.Add("Notion-Version", "2022-06-28");
+
+            var response = client.Send(request);
+
+            // StatusCode를 포함한 Header 출력
+            Console.WriteLine(response);
+            // JSON 형식의 Body 출력
+            var content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            Console.WriteLine(content);
+
+            var parsed = JsonSerializer.Deserialize<PageInformation>(content);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        private static bool Update(string baseUri, string pageKey, string APIKey)
+        {
+            HttpClient client = new();
+
+            // https://developers.notion.com/reference/patch-page 문서 내용에 따라 Patch로 request 작성
+            var request = new HttpRequestMessage(HttpMethod.Patch, $"{baseUri}/{pageKey}");
+            request.Headers.Add("Authorization", $"Bearer {APIKey}");
+            request.Headers.Add("Notion-Version", "2022-06-28");
+            var queryItem = new QueryPageItem();
+            var properties = new Dictionary<string, PageProperty>()
+            {
+                { "선택" , new PageSelect() { select = new() { name = "1" } } }
+            };
+            queryItem.properties = properties;
+            request.Content = JsonContent.Create(queryItem);
+
+            var response = client.Send(request);
+
+            // StatusCode를 포함한 Header 출력
+            Console.WriteLine(response);
+            // JSON 형식의 Body 출력
+            var content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            Console.WriteLine(content);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        // Notion API는 페이지 삭제를 지원하지 않는다.
+        private static bool Archieve(string baseUri, string pageKey, string APIKey)
+        {
+            HttpClient client = new();
+
+            // https://developers.notion.com/reference/archive-a-page 문서 내용에 따라 Patch로 request 작성
+            var request = new HttpRequestMessage(HttpMethod.Patch, $"{baseUri}/{pageKey}");
+            request.Headers.Add("Authorization", $"Bearer {APIKey}");
+            request.Headers.Add("Notion-Version", "2022-06-28");
+            var queryItem = new QueryPageItem();
+            queryItem.archived = false;
+            request.Content = JsonContent.Create(queryItem);
+
+            var response = client.Send(request);
+
+            // StatusCode를 포함한 Header 출력
+            Console.WriteLine(response);
+            // JSON 형식의 Body 출력
+            var content = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+            Console.WriteLine(content);
 
             return response.StatusCode == System.Net.HttpStatusCode.OK;
         }
