925ef34edc50b5800c207e0c13412bc7c3c7b767
peponi <peponi_paradise@naver.com>
2024-06-25 15:52:22 +0900

    .Net framework용 placeholder textbox 업로드

---
+++ WinForms/PlaceholderTextBoxControl/PlaceholderTextBoxControl/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PlaceholderTextBoxControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.placeholderTextBox1 = new PlaceholderTextBoxControl.PlaceholderTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 160);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Escape focus";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // placeholderTextBox1
+            // 
+            this.placeholderTextBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.placeholderTextBox1.Location = new System.Drawing.Point(155, 149);
+            this.placeholderTextBox1.Name = "placeholderTextBox1";
+            this.placeholderTextBox1.PlaceholderText = "Input text";
+            this.placeholderTextBox1.PlaceholderTextColor = System.Drawing.Color.DarkGray;
+            this.placeholderTextBox1.Size = new System.Drawing.Size(227, 21);
+            this.placeholderTextBox1.TabIndex = 2;
+            this.placeholderTextBox1.Text = "Input text";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.placeholderTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private PlaceholderTextBox placeholderTextBox1;
+    }
+}
+
---
+++ WinForms/PlaceholderTextBoxControl/PlaceholderTextBoxControl/PlaceholderTextBox.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PlaceholderTextBoxControl
+{
+    public class PlaceholderTextBox : TextBox
+    {
+        [Category("Placeholder")]
+        [Description("Set placeholder text")]
+        public string PlaceholderText
+        {
+            get => _placeholderText;
+            set
+            {
+                UpdateText(true);
+                _placeholderText = value;
+                UpdateText(false);
+            }
+        }
+
+        [Category("Placeholder")]
+        [Description("Set placeholder text color")]
+        public Color PlaceholderTextColor
+        {
+            get => _placeholderColor;
+            set
+            {
+                _placeholderColor = value;
+                UpdateColor();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("A Color that represents the control's foreground color.")]
+        public new Color ForeColor
+        {
+            get => _foreColor;
+            set
+            {
+                _foreColor = value;
+                UpdateColor();
+            }
+        }
+
+        private string _placeholderText = "Input text";
+        private Color _placeholderColor = Color.DarkGray;
+        private Color _foreColor = SystemColors.WindowText;
+
+        public PlaceholderTextBox()
+        {
+            GotFocus += (s, e) => { UpdateSetting(); };
+            LostFocus += (s, e) => { UpdateSetting(); };
+            TextChanged += (s, e) => { UpdateSetting(); };
+
+            UpdateText();
+            UpdateColor();
+        }
+
+        private void UpdateSetting()
+        {
+            UpdateText(Focused);
+            UpdateColor();
+        }
+
+        private void UpdateText(bool focused = false)
+        {
+            if (focused && string.Equals(Text, PlaceholderText))
+                Text = string.Empty;
+            else if (!focused && string.IsNullOrWhiteSpace(Text))
+                Text = PlaceholderText;
+        }
+
+        private void UpdateColor()
+        {
+            if (Focused)
+                base.ForeColor = ForeColor;
+            else
+            {
+                if (string.Equals(Text, PlaceholderText))
+                    base.ForeColor = PlaceholderTextColor;
+                else
+                    base.ForeColor = ForeColor;
+            }
+        }
+    }
+}
