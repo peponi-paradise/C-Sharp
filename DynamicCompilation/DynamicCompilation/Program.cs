8264e99ccdc4e9a8112cb8e7ff8176ba39589ed6
peponi <peponi_paradise@naver.com>
2025-05-19 14:06:05 +0900

    동적 코드 호출 연습 (Roslyn, RunAsync, EvaluateAsync)
    스크립트에서 메서드를 불러오고, 파라미터 연계하여 계산하는 방법 연습
    
    Roslyn 이용하여 실행 (Microsoft.CodeAnalysis.CSharp.Scripting)

---
+++ DynamicCompilation/DynamicCompilation/Program.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace DynamicCompilation;
+
+public class RunAsyncTest
+{
+    public List<double> Parameters;
+
+    public string ComputationCode;
+
+    private Script _script;
+
+    public RunAsyncTest(List<double> parameters, string code)
+    {
+        Parameters = parameters;
+        ComputationCode = CreateCode(code);
+
+        _script = CreateScript();
+    }
+
+    public Task<object> RunScriptAsync(AdditionalData additional) => Task.Run(() => _script.RunAsync(additional).Result.ReturnValue);
+
+    private string CreateCode(string code)
+    {
+        string parameters = string.Empty;
+        for (int i = 0; i < Parameters.Count; i++)
+        {
+            parameters += $"double P{i + 1} = {Parameters[i]};";
+        }
+        return parameters + code;
+    }
+
+    private Script CreateScript()
+    {
+        var options = ScriptOptions.Default;
+        options = options.AddImports("System");
+
+        var script = CSharpScript.Create(ComputationCode, options, typeof(AdditionalData));
+        script.Compile();
+
+        return script;
+    }
+}
+
+public record AdditionalData(int X, double Y);
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Start");
+        Console.WriteLine();
+
+        var runAsync = new RunAsyncTest([1, 2, 3], GetRunAsyncCode());
+        var sum = await runAsync.RunScriptAsync(new(10, 7));
+
+        Console.WriteLine($"RunAsync() result : {sum}");
+
+        var evaluateAsync = await CSharpScript.EvaluateAsync(GetEvaluateAsyncCode(), globals: new AdditionalData(5, 7));
+
+        Console.WriteLine($"EvaluateAsync() result : {evaluateAsync}");
+
+        Console.WriteLine();
+        Console.WriteLine("End");
+    }
+
+    private static string GetRunAsyncCode()
+    {
+        return "return P1 + Math.Pow(P2, P3) + X - Y;";
+    }
+
+    private static string GetEvaluateAsyncCode()
+    {
+        return "return X - Y;";
+    }
+}
